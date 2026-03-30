82c9972065e8fc7b986f9caa05f75b0592624151
Lachu Leo <lakshmanan.unq@gmail.com>
2026-03-30 23:22:26 +0530

    add root route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("API running 🚀")
+})
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected ✅"))
   .catch(err => console.log(err))
